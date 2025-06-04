f4b716d4029fcf1a33e4ec59519d996c8b643ee2
Ajaykumar-6209 <ajaygoud6333@gmail.com>
2025-06-04 22:35:00 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,10 @@
 public class main {
     public static void main(String[] args) {
-        int i = 1;
-        while(i < 10) {
+        int i = 100;
+          while(i > 1) {
             System.out.println(i);
          
-            i++;
+            i--;
         }
 
     }
